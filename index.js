531943973ff4834f6cbea4caaf1522e5e5735608
Erica Lee <erica.lee@students.olin.edu>
2019-03-01 17:15:51 -0500

    ADD: more dependencies

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const express = require('express')
-const app = express()
+// server.js
+const next = require('next')
+const routes = require('./routes')
+const app = next({dev: process.env.NODE_ENV !== 'production'})
+const handler = routes.getRequestHandler(app)
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(3000, () => console.log('Server ready'))
+// With express
+const express = require('express')
+app.prepare().then(() => {
+  express().use(handler).listen(3000)
+})
